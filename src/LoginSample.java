f8681e8481872a60ae11a5260cdf586f095fffcf
josuemj <98498772+josuemj@users.noreply.github.com>
2023-09-14 21:42:12 -0300

    branch1

---
+++ src/LoginSample.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class LoginSample extends JFrame {
+
+    private JPanel loginWindow;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+
+    public LoginSample(){
+        setContentPane(loginWindow);
+        setSize(220,200);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame myWindow = new LoginSample();
+        myWindow.setVisible(true);
+    }
+
+}
